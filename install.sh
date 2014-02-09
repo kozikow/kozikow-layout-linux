28f40a0512e04cc9dac3567fef58d623b1708eb8
Robert Kozikowski <r.kozikowski@gmail.com>
2014-02-09 16:47:35 +0000

    Created install.sh and README file

--- install.sh
+++ install.sh
@@ -4,11 +4,18 @@ sudo apt-get install python-dev
 sudo apt-get install python-pip
 
 
+git clone https://github.com/alols/xcape.git
+cd xcape
+make
+sudo cp xcape /bin/
+cd ..
+
+
 # latest development version of evdev. Stable version didn't work for me.
 # In case of some errors check out http://pythonhosted.org/evdev/install.html
 pip install git+git://github.com/gvalkov/python-evdev.git
 
 pip install pyinotify
 pip install sh
-
-
+pip install pyudev
+pip install evdev
